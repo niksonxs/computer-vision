29c7485823b95a7fd049d0d20b184f9e41ab585a
unknown <nicolae.dubenco@isa.utm.md>
2021-10-09 17:15:48 +0300

    moved inside folder

--- homework_1/script.py
+++ homework_1/script.py
@@ -9,14 +9,14 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.2, 5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-    eyes = eye_cascade.detectMultiScale(gray, 1.2, 5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    faces = face_cascade.detectMultiScale(gray, 2, 7, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    eyes = eye_cascade.detectMultiScale(gray, 1.2, 7, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
     for (x,y,w,h) in eyes:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(120,90,60),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 245, 67), 2)
 
-    cv2.imshow("Face'n eyes detector", frame)
+    cv2.imshow("Detector de fete si ochisori frumosi xD", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
